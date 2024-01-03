c2a835df77bbb6b37c840eb7e829dd6ab3eea152
Maria-Faria <mariaeduardadefaria15@gmail.com>
2024-01-02 22:49:18 -0300

    primeiro exercicio em c#

---
+++ Ex1/Ex1/Program.cs
@@ -0,0 +1,15 @@
+﻿//1. Criar uma variável chamada curso, e guarde nela o nome do curso estudado.
+string curso = "C#: Criando sua primeira aplicação";
+
+//2. Mostrar o conteúdo da variável curso no console.
+Console.WriteLine(curso);
+
+//3. Criar uma variável chamada nome e outra chamada sobrenome e guardar nelas seu nome e sobrenome.
+string nome = "Maria Eduarda ";
+string sobrenome = "de Faria";
+
+//Console.WriteLine(nome + sobrenome);
+
+//4. Mostrar na tela o nome dos instrutores do curso.
+Console.WriteLine("--------------------------------");
+Console.WriteLine("Instrutores:\nDaniel Portugal\nGuilherme Lima");
