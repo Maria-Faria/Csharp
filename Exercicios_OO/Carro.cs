18a92a481863bbe22da00a032f0c895bcbda24f4
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-02-21 00:29:47 -0300

    criação de classes e métodos

---
+++ Exercicios_OO/Carro.cs
@@ -0,0 +1,28 @@
+﻿class Carro
+{
+    public string modelo;
+    public string cor;
+    public int ano;
+   
+    public void Acelerar()
+    {
+        Console.WriteLine("O carro está acelerando...");
+    }
+
+    public void Frear()
+    {
+        Console.WriteLine("O carro está freando...");
+    }
+
+    public void Buzinar()
+    {
+        Console.WriteLine("Bi bi bi");
+    }
+
+    public void ExibirFicha()
+    {
+        Console.WriteLine($"Modelo: {modelo}");
+        Console.WriteLine($"Cor: {cor}");
+        Console.WriteLine($"Ano: {ano}");
+    }
+}
